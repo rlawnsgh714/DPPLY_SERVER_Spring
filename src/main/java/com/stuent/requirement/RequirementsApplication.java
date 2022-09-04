af417cf1dc039a8c1a3009a52fc926f801d10228
김준호 <80739316+rlawnsgh714@users.noreply.github.com>
2022-09-04 17:37:56 +0900

    :tada: :: project start

---
+++ src/main/java/com/stuent/requirement/RequirementsApplication.java
@@ -0,0 +1,13 @@
+package com.stuent.requirement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequirementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stuent/requirement/RequirementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuent.requirement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequirementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
