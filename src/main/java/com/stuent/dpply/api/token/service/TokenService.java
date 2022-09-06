8e155d48ae034306a715d8b54a86c0adc652a103
김준호 <80739316+rlawnsgh714@users.noreply.github.com>
2022-09-07 00:24:41 +0900

    :memo: :: verify toke feature

--- src/main/java/com/stuent/dpply/api/token/service/TokenService.java
+++ src/main/java/com/stuent/dpply/api/token/service/TokenService.java
@@ -1,8 +1,11 @@
 package com.stuent.dpply.api.token.service;
 
+import com.stuent.dpply.api.auth.domain.entity.Auth;
 import com.stuent.dpply.api.token.domain.enums.JWT;
 
 public interface TokenService {
 
     String generateToken(String id, JWT jwt);
+
+    Auth verifyToken(String token);
 }
--- src/main/java/com/stuent/dpply/api/token/service/TokenServiceImpl.java
+++ src/main/java/com/stuent/dpply/api/token/service/TokenServiceImpl.java
@@ -1,7 +1,15 @@
 package com.stuent.dpply.api.token.service;
 
+import com.stuent.dpply.api.auth.domain.entity.Auth;
+import com.stuent.dpply.api.auth.domain.repository.AuthRepository;
 import com.stuent.dpply.api.token.domain.enums.JWT;
 import com.stuent.dpply.common.config.properties.AppProperties;
+import com.stuent.dpply.common.exception.BadRequestException;
+import com.stuent.dpply.common.exception.GoneException;
+import com.stuent.dpply.common.exception.InternalServerException;
+import com.stuent.dpply.common.exception.NotFoundException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +27,7 @@ public class TokenServiceImpl implements TokenService{
     private final AppProperties appProperties;
     private final long JWT_ACCESS_EXPIRE = 60 * 60 * 1000;
     private final long JWT_REFRESH_EXPIRE = 60 * 60 * 1000 * 24 * 7;
+    private final AuthRepository authRepository;
 
     @Override
     public String generateToken(String id, JWT jwt) {
@@ -34,7 +43,7 @@ public class TokenServiceImpl implements TokenService{
         }
 
         Map<String, Object> claims = new HashMap<>();
-        claims.put("authID", id);
+        claims.put("authId", id);
 
         return Jwts.builder()
                 .setClaims(claims)
@@ -43,5 +52,30 @@ public class TokenServiceImpl implements TokenService{
                 .setExpiration(expiredAt)
                 .signWith(signatureAlgorithm, secretKey)
                 .compact();
+    }
+
+    private Claims parseToken(String token, JWT jwt) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(
+                            jwt == JWT.ACCESS ?
+                                    appProperties.getACCESS_SECRET() : appProperties.getREFRESH_SECRET()
+                    )
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw new GoneException("토큰이 만료되었습니다");
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException("토큰이 없습니다");
+        } catch (Exception e) {
+            throw new InternalServerException("토큰 해석 중 에러 발생");
+        }
+    }
+
+    @Override
+    public Auth verifyToken(String token) {
+        return authRepository.findById(
+                parseToken(token, JWT.ACCESS).get("authId").toString())
+                .orElseThrow(() -> new NotFoundException("해당 아이디는 존재하지 않습니다"));
     }
 }
