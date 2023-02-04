7487fecc6a20132bab1efdf3cbc9fb2824100ec9
김준호 <80739316+rlawnsgh714@users.noreply.github.com>
2023-02-05 00:40:33 +0900

    :memo: :: fix config

--- src/main/java/com/stuent/dpply/common/config/webmvc/WebMVCConfig.java
+++ src/main/java/com/stuent/dpply/common/config/webmvc/WebMVCConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebMVCConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
 
